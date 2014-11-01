7d5081695415350e2ed3004ca618d3a553296996
Peter Bergman <peter.bergman@live.se>
2014-11-01 21:14:22 +0100

    Changed to a thread-pool

--- mfserver/src/main/java/org/macfinder/ClientConnection.java
+++ mfserver/src/main/java/org/macfinder/ClientConnection.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
  */
 public class ClientConnection implements Runnable {
 
-	private static final Logger LOGGER = Logger.getLogger(ClientConnection.class.getName());
+	private final static Logger LOGGER = Logger.getLogger(ClientConnection.class.getName());
 	static {
 		LOGGER.setUseParentHandlers(false);
 		LOGGER.addHandler(new ConsoleHandler());
@@ -21,44 +21,61 @@ public class ClientConnection implements Runnable {
 	private Thread internalThread;
 	private Socket socket;
 
+	/**
+	 * Constructs a new ClientConnection-object.
+	 *
+	 * @param socket	the socket with the connection to the client.
+	 */
 	public ClientConnection(Socket socket) {
 		this.socket = socket;
 		internalThread = new Thread(this);
 		internalThread.start();
 	}
 
+	/**
+	 * Method to execute upon client connections.
+	 *
+	 * Extracts the HTTP-request from the socket connection,
+	 * parses the request and then passes the request on the the
+	 * correct service.
+	 */
 	public void run() {
 		while (!internalThread.isInterrupted()) {
-			try {
-				LOGGER.info("Connection active!");
-				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String line;
-				StringBuilder request = new StringBuilder();
-				while ((line = reader.readLine()) != null) {
-					request.append(line + "\n");
+			LOGGER.info("Connection active!");
+			String request = parseRequest();
+			if (validRequest(request)) {
+				String[] requestArray = request.split("\n\n");
+				String headers = requestArray[0];
+				String data = requestArray[1];
+				if (requestType(headers).equals(RequestType.AGENT)) {
+					// TODO: handle agent stuff
+				} else if (requestType(headers).equals(RequestType.CLIENT)) {
+					// TODO: handle client stuff
 				}
-				if (validRequest(request.toString())) {
-					String[] requestArray = request.toString().split("\n\n");
-					String headers = requestArray[0];
-					String data = requestArray[1];
-					if (requestType(headers).equals(RequestType.AGENT)) {
-
-					} else if (requestType(headers).equals(RequestType.CLIENT)) {
-						// TODO: implement
-					}
-				}
-				internalThread.interrupt();
-			} catch (IOException ioe) {
-				LOGGER.severe(ioe.toString());
 			}
+			internalThread.interrupt();
 		}
+	}
+
+	private String parseRequest() {
+		StringBuilder request = new StringBuilder();
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				request.append(line + "\n");
+			}
+		} catch (IOException ioe) {
+			LOGGER.severe(ioe.toString());
+		}
+		return request.toString();
 	}
 
 	private boolean validRequest(String request) {
 		return (request.contains("/agent") || request.contains("/client")) && request.contains("/n/n");
 	}
 
-	private String requestType(String headers) {
-		return (headers.contains("/agent") ? "agent" : "client");
+	private RequestType requestType(String headers) {
+		return (headers.contains("/agent") ? RequestType.AGENT : RequestType.CLIENT);
 	}
 }
