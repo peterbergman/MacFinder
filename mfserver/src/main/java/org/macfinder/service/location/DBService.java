42ff477ed6145ef93048a83036bff54ae100ce25
Peter Bergman <peter.bergman@live.se>
2014-11-02 11:43:09 +0100

    Started working on a DBService

---
+++ mfserver/src/main/java/org/macfinder/service/location/DBService.java
@@ -0,0 +1,22 @@
+package org.macfinder.service.location;
+
+/**
+ * Class to handle database operations
+ */
+public class DBService {
+
+	private final static String DB_URL = "";
+
+	public DBService() {
+
+	}
+
+	public void put() {
+
+	}
+
+	/*public Location get() {
+
+	}*/
+
+}
