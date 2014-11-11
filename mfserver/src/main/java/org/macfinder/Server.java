25f5e0861e82ff562cf636c33f54a259e0fad0e3
Peter Bergman <peter.bergman@live.se>
2014-11-11 19:41:39 +0100

    Javadoc update + change in arguments for constructor

--- mfserver/src/main/java/org/macfinder/Server.java
+++ mfserver/src/main/java/org/macfinder/Server.java
@@ -19,21 +19,35 @@ public class Server {
 		LOGGER.setUseParentHandlers(false);
 		LOGGER.addHandler(new ConsoleHandler());
 	}
-	private final static int PORT = 8080;
-	private final static int QUEUE_LENGTH = 50;
-	private final static int THREAD_COUNT = 4;
+
 	private ServerSocket serverSocket;
 	private ExecutorService pool;
+	private int port, queueLength;
 
-	public Server() {
-		pool = Executors.newFixedThreadPool(THREAD_COUNT);
+	/**
+	 * Constructs a new Server object.
+	 *
+	 * @param port			the port to be used when opening the server
+	 * @param queueLength	the queue length to be used when opening the server
+	 * @param threadCount	the number of concurrent active threads to handle connections
+	 */
+	public Server(int port, int queueLength, int threadCount) {
+		pool = Executors.newFixedThreadPool(threadCount);
+		this.port = port;
+		this.queueLength = queueLength;
 	}
-	
-	public void open() {
+
+	/**
+	 * Method to start the server.
+	 * <p></p>
+	 * Opens the server for connections on the specified port
+	 * with the specified queue length.
+	 */
+	public void start() {
 		LOGGER.info("Trying to start server...");
 		try {
 			InetAddress localHost = InetAddress.getLocalHost();
-			serverSocket = new ServerSocket(PORT, QUEUE_LENGTH, localHost);
+			serverSocket = new ServerSocket(port, queueLength, localHost);
 			LOGGER.info("Server started!");
 			while (true) {
 				Socket socket = serverSocket.accept();
@@ -45,7 +59,10 @@ public class Server {
 		}
 	}
 
-	public void close() {
+	/**
+	 * Metohd to stop the server.
+	 */
+	public void stop() {
 		if (serverSocket != null) {
 			try {
 				serverSocket.close();
