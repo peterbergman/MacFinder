af0f7163b12e716c7c7169bbb1a517c3e303a947
Peter Bergman <peter.bergman@live.se>
2014-11-11 19:59:48 +0100

    Updated model classes to match the ones on the server side

--- mfagent/src/main/java/org/macfinder/model/Ping.java
+++ mfagent/src/main/java/org/macfinder/model/Ping.java
@@ -1,12 +1,11 @@
 package org.macfinder.model;
 
+
 import java.util.Date;
 import java.util.List;
 
 /**
  * Class to represent a ping sent from a machine.
- * This class just acts as a data container that can be
- * converted to JSON.
  */
 public class Ping {
 
@@ -31,4 +30,62 @@ public class Ping {
 	public Date getTimestamp() {
 		return timestamp;
 	}
+
+	/**
+	 * Compares this Ping to the specified object.
+	 * <p></p>
+	 * The result is true if and only if the argument is not null
+	 * and is a Ping object with the same list of AccessPoints as this Ping.
+	 * The list of AccessPoints is compared by checking that this Ping's
+	 * AccessPoints contains all of the other Ping's AccessPoints and vice versa.
+	 * <p></p>
+	 * If both Pings does not contain any AccessPoints, then the timestamps
+	 * are compared and the result ist true if both timestamps are equal.
+	 *
+	 * @param other	the object to compare this Ping against
+	 * @return		true if this Ping's list of AccessPoints
+	 * 				contains all of the AccessPoints in the
+	 * 				given Ping's list of AccessPoints and vice versa,
+	 * 				false otherwise. If both Pings does not contain any
+	 * 				AccessPoints, then true if both Ping's timestamp are equal.
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other != null && other instanceof Ping) {
+			Ping otherPing = (Ping) other;
+			if (wifiAccessPoints.size() != 0 && otherPing.getWifiAccessPoints().size() != 0) {
+				return wifiAccessPoints.containsAll(otherPing.getWifiAccessPoints()) &&
+						otherPing.getWifiAccessPoints().containsAll(wifiAccessPoints);
+			} else {
+				return timestamp.equals(otherPing.getTimestamp());
+			}
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Returns the hash code of the Ping.
+	 * <p></p>
+	 * The hash code is calculated by calling adding the result
+	 * from calling hashCode on each AccessPoint together.
+	 * <p></p>
+	 * If the Ping does not contain any AccessPoints, then the
+	 * hash code is calculated by calling hashCode on the timestamp.
+	 *
+	 * @return a hash code value for this object
+	 */
+	@Override
+	public int hashCode() {
+		if (wifiAccessPoints.size() != 0) {
+			int hashCode = 0;
+			for (AccessPoint accessPoint : wifiAccessPoints) {
+				hashCode += accessPoint.hashCode();
+			}
+			return hashCode;
+		} else {
+			return timestamp.hashCode();
+		}
+	}
+
 }
--- mfagent/src/main/java/org/macfinder/model/User.java
+++ mfagent/src/main/java/org/macfinder/model/User.java
@@ -7,10 +7,17 @@ import java.util.List;
  * Class to represent a user.
  */
 public class User {
+
 	private String username;
 	private String password;
 	private List<Machine> machines;
 
+	/**
+	 * Constructs a new User object.
+	 *
+	 * @param username	the username of the user.
+	 * @param password	the password of the user.
+	 */
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
@@ -25,13 +32,44 @@ public class User {
 		return password;
 	}
 
-	public void addMachine(Machine machine) {
-		machines.add(machine);
-	}
-
 	public List<Machine> getMachines() {
-		return new ArrayList<Machine>(machines);
+		return machines;
 	}
 
+	/**
+	 * Method to add a Machine to a User.
+	 *<p></p>
+	 * Initiates the machine list if null and adds
+	 * the new machine to the list.
+	 * <p></p>
+	 * If the new machine is already present in the User's
+	 * list of machines, then the Ping from the new machine
+	 * is added to the existing machine. This also assumes that
+	 * the machine only contains one Ping to add to the existing
+	 * machine.
+	 * <p></p>
+	 * If the machine is not present in the User's list of machines,
+	 * then the new machine is added to the list.
+	 *
+	 * @param machine
+	 */
+	public void addMachine(Machine machine) {
+		boolean foundMachine = false;
+		if (machines == null) {
+			machines = new ArrayList<Machine>();
+			machines.add(machine);
+		} else {
+			for (Machine existingMachine : machines) {
+				if (existingMachine.equals(machine)) {
+					existingMachine.addPing(machine.getPings().get(0));
+					foundMachine = true;
+					break;
+				}
+			}
+			if (!foundMachine) {
+				machines.add(machine);
+			}
+		}
+	}
 
 }
