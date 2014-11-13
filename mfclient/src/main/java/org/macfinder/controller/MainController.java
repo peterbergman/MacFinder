dda510fbee022882db627bc92374510be165631c
Peter Bergman <peter.bergman@live.se>
2014-11-14 00:30:32 +0100

    Added logic to send request to get the location of a machine

--- mfclient/src/main/java/org/macfinder/controller/MainController.java
+++ mfclient/src/main/java/org/macfinder/controller/MainController.java
@@ -1,6 +1,10 @@
 package org.macfinder.controller;
 
+import com.google.gson.Gson;
+import org.macfinder.model.Machine;
 import org.macfinder.model.User;
+import org.macfinder.model.http.HTTPResponse;
+import org.macfinder.utility.ServerConnection;
 import org.macfinder.view.MainView;
 
 import javax.swing.*;
@@ -8,17 +12,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class to act as a controller for the main view.
  */
 public class MainController {
 
+	private final static Gson GSON = new Gson();
+
 	private MainView mainView;
 	private User user;
 
 	public MainController(User user) {
 		mainView = new MainView();
+		mainView.addLookupButtonActionListener(new LookupButtonActionActionListener());
 		mainView.setMachines(user.getMachines());
 		this.user = user;
 	}
@@ -40,7 +49,14 @@ public class MainController {
 	private class LookupButtonActionActionListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent event) {
-
+			List<Machine> selectedMachine = new ArrayList<Machine>();
+			selectedMachine.add(mainView.getSelectedMachine());
+			if (selectedMachine != null) {
+				user.getMachines().retainAll(selectedMachine);
+				HTTPResponse response = ServerConnection.sendData(user);
+				user = GSON.fromJson(response.getBody(), User.class);
+				System.out.println(user);
+			}
 		}
 	}
 }
