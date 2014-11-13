bf7fa98c7d6ca1c6929dae8f299fcf479c705189
Peter Bergman <peter.bergman@live.se>
2014-11-13 22:06:36 +0100

    Update in the client

--- mfclient/src/main/java/org/macfinder/utility/ServerConnection.java
+++ mfclient/src/main/java/org/macfinder/utility/ServerConnection.java
@@ -2,7 +2,7 @@ package org.macfinder.utility;
 
 import com.google.gson.Gson;
 import org.macfinder.model.User;
-import org.macfinder.model.http.HTTPRequest;
+import org.macfinder.model.http.HTTPResponse;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -15,10 +15,9 @@ import java.net.URLEncoder;
  */
 public class ServerConnection {
 
-	//private static final String SERVER_ADDRESS = "https://3cxivtljvvbg.runscope.net"; //Runscope
-	//private static final String SERVER_ADDRESS = "http://192.168.1.74:8080/client"; //Home
-	private static final String SERVER_ADDRESS = "http://130.237.241.80:8080/agent"; //DSV
-
+	//private static final String SERVER_ADDRESS = "https://92hn1nhwh76y.runscope.net"; //Runscope
+	private final static String SERVER_ADDRESS = "http://192.168.1.74:8080/agent"; //Home
+	//private static final String SERVER_ADDRESS = "http://77.238.33.55:8080/agent"; //DSV
 
 	/**
 	 * Sends data to the MacFinder server.
@@ -29,10 +28,10 @@ public class ServerConnection {
 	 *              		needed to communicate with the server
 	 * @throws IOException	if the internal HttpURLConnection fails for any reason
 	 */
-	public static User sendData(User data) {
+	public static HTTPResponse sendData(User data) {
 		Gson gson = new Gson();
 		String jsonData = gson.toJson(data);
-		User existingUser = null;
+		HTTPResponse response = null;
 		try {
 			jsonData = URLEncoder.encode(jsonData, "utf-8");
 			URL url = new URL(SERVER_ADDRESS);
@@ -41,48 +40,36 @@ public class ServerConnection {
 			connection.setDoOutput(true);
 			connection.setFixedLengthStreamingMode(jsonData.getBytes().length);
 			connection.setRequestProperty("Content-Length", jsonData.getBytes().length + "");
-			connection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-			//connection.connect();
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Charset", "utf-8");
 			OutputStream outputStream = new BufferedOutputStream(connection.getOutputStream());
 			outputStream.write(jsonData.getBytes());
 			outputStream.flush();
+			response = parseResponse(connection);
 			outputStream.close();
-			HTTPRequest request = parseRequest(connection);
-			existingUser = gson.fromJson(request.getBody(), User.class);
 			connection.disconnect();
 		} catch (IOException ioe) {
 
 		}
-		return existingUser;
+		return response;
 	}
 
 	/**
 	 * Helper method to parse the HTTP response from the server.
 	 *
-	 * @return				a HTTPRequest object with the body and headers separated
+	 * @param connection	the connection object to read the response from
+	 * @return				a HTTPResponse object with the parsed body of the response
 	 * @throws IOException	if something went wrong opening the stream from the server
 	 */
-	private static HTTPRequest parseRequest(HttpURLConnection connection) throws IOException {
-		HTTPRequest request = null;
-		StringBuilder stringBuilder = new StringBuilder();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String line;
-
-		while ((line = reader.readLine()).length() != 0) {
-			stringBuilder.append(line + " ");
-		}
-		request = new HTTPRequest();
-		request.setHeaders(stringBuilder.toString().replaceAll(":", "").split(" "));
-		stringBuilder.setLength(0);
-		int contentLength = Integer.parseInt(request.getHeaderValue("Content-Length"));
-
-		for (int i = 0; i < contentLength; i++) {
-			stringBuilder.append((char)reader.read());
-		}
-		reader.close();
-		String decodedBody = URLDecoder.decode(stringBuilder.toString(), "utf-8");
-		request.setBody(decodedBody);
-		return request;
+	private static HTTPResponse parseResponse(HttpURLConnection connection) throws IOException {
+		HTTPResponse response = new HTTPResponse();
+		response.setStatusCode(connection.getResponseCode());
+		String[] responseString = connection.getResponseMessage().split("\\\\r\\\\n\\\\r\\\\n");
+		String responseHeaders = responseString[0];
+		response.setHeaders(responseHeaders);
+		String responseData = response.getStatusCode() == 200 ? responseString[1] : "";
+		response.setBody(URLDecoder.decode(responseData, "utf-8"));
+		return response;
 	}
 
 }
