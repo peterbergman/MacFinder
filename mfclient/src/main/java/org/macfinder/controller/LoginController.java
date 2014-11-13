752b20e4f61557bc0e4b71c202541779b151a4b5
Peter Bergman <peter.bergman@live.se>
2014-11-13 22:27:49 +0100

    Added working login logic to controller

--- mfclient/src/main/java/org/macfinder/controller/LoginController.java
+++ mfclient/src/main/java/org/macfinder/controller/LoginController.java
@@ -1,10 +1,13 @@
 package org.macfinder.controller;
 
+import com.google.gson.Gson;
 import org.macfinder.model.User;
+import org.macfinder.model.http.HTTPResponse;
 import org.macfinder.utility.ServerConnection;
 import org.macfinder.view.LoginView;
 import org.macfinder.view.MainView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -13,6 +16,8 @@ import java.io.IOException;
  * Class to act as a controller for the login view.
  */
 public class LoginController {
+
+	private final static Gson GSON = new Gson();
 
 	private LoginView loginView;
 
@@ -32,11 +37,14 @@ public class LoginController {
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			User user = new User(loginView.getUsername(), loginView.getPassword());
-			User existingUser = ServerConnection.sendData(user);
-			if (loginView.getUsername().equals("") && loginView.getPassword().equals("")) {
+			HTTPResponse response = ServerConnection.sendData(user);
+			if (response.getStatusCode() == 200) {
 				loginView.close();
-				MainController mainController = new MainController();
+				user = GSON.fromJson(response.getBody(), User.class);
+				MainController mainController = new MainController(user);
 				mainController.start();
+			} else {
+				JOptionPane.showMessageDialog(loginView, "Login failed!", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 		}
 	}
