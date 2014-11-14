ce6e2b6d26d385361818bc6eb53f2aa28c8cde7c
Peter Bergman <peter.bergman@live.se>
2014-11-14 01:17:30 +0100

    Added getters for lat and long

--- mfserver/src/main/java/org/macfinder/model/GeoLookup.java
+++ mfserver/src/main/java/org/macfinder/model/GeoLookup.java
@@ -8,11 +8,27 @@ public class GeoLookup {
 	private Location location;
 	private double accuracy;
 
+	public double getLatitude() {
+		return location.getLatitude();
+	}
+
+	public double getLongitude() {
+		return location.getLongitude();
+	}
+
 	/**
 	 * Class to represent a location that contains latitude and longitude.
 	 */
 	private class Location {
 		private double lng, lat;
+
+		public double getLatitude() {
+			return lat;
+		}
+
+		public double getLongitude() {
+			return lng;
+		}
 	}
 
 }
